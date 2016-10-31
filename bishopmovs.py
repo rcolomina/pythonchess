5794fa725479907d5680993b4a837fdcdc33768d
rcolomina <rcolomina@gmail.com>
2016-10-31 00:28:53 +0000

    Added new function to list targets on bishops

--- bishopmovs.py
+++ bishopmovs.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+
+from piece import Piece
+from gameNode import GameNode        
+
 # Inputs: This function accept a coordenate of a bishop piece located at coord and a set of 
 #         all pieces of the game ocupy
 # Return: A list of all possible movements i.e pairs [x,y] not capturing pieces are returnet
@@ -57,3 +62,63 @@ def listMovBishop(coord,ocupyCells):
     listMovs.extend(d2)
     
     return listMovs
+
+
+
+
+# Inputs: This function accept a coordenate of bishop piece located at coord and a game node
+#
+# Return: A list of all possible adversary captures for an input bishop i.e pairs [x,y] 
+def listTargetsBishop(gameNode,piece):        
+
+    listOfTargets=[]
+    coord=piece.coordenates
+
+    cX=piece.coordenates[0]
+    cY=piece.coordenates[1]
+    
+    # First Diagonal \
+    #Diagonal1: Equations that rules diagonal are -->  d=y+x => y=d-x
+    diff=cY+cX
+    firstDiagonalTargets=[p for p in gameNode.allPieces if p.coordenates[1]==diff-p.coordenates[0]]
+
+    # print piecesDiagonal
+    firstDiagonalTargets.sort(key=lambda x:x.coordenates,reverse=False)
+
+    if len(firstDiagonalTargets)>0:
+        index = firstDiagonalTargets.index(piece)
+        previous_value = firstDiagonalTargets[index-1] if index-1>=0 else None
+        next_value = firstDiagonalTargets[index+1] if index+1<len(firstDiagonalTargets) else None
+        
+        if previous_value!=None:            
+            if piece.colour()!=previous_value.colour():
+                listOfTargets.append(previous_value)
+
+        if next_value!=None:
+            if piece.colour()!=next_value.colour():
+                listOfTargets.append(next_value)        
+
+    # Second Diagonal    
+    #Diagonal1: Equations that rules diagonal are -->  d=y-x => y=x+d
+    diff=cY-cX
+    secondDiagonalTargets=[p for p in gameNode.allPieces if p.coordenates[1]==diff+p.coordenates[0]]
+                
+    # print piecesDiagonal
+    secondDiagonalTargets.sort(key=lambda x:x.coordenates,reverse=False)
+
+    if len(secondDiagonalTargets)>0:
+        index = secondDiagonalTargets.index(piece)
+        previous_value = secondDiagonalTargets[index-1] if index-1>=0 else None
+        next_value = secondDiagonalTargets[index+1] if index+1<len(secondDiagonalTargets) else None
+        
+        if previous_value!=None:            
+            if piece.colour()!=previous_value.colour():
+                listOfTargets.append(previous_value)
+
+        if next_value!=None:
+            if piece.colour()!=next_value.colour():
+                listOfTargets.append(next_value)        
+
+                
+    return listOfTargets
+    
