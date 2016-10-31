32a54e919550b9ad5e1180f7f0d8aea375b11350
rcolomina <rcolomina@gmail.com>
2016-10-31 00:49:30 +0000

    Rewritten of game node constructor

--- rookmovs_test.py
+++ rookmovs_test.py
@@ -1,39 +1,47 @@
+#!/usr/bin/python
+
 from piece import Piece
 from gameNode import GameNode        
-from functions import *
+#from functions import *
 from minmax import MinMax
+from rookmovs import *
 
 print "Checking GameNode class constructur"
 
 listPiecesWhite=[]
 listPiecesBlack=[]
-listPiecesWhite.append(Piece('Q',[4,3]))
-listPiecesWhite.append(Piece('N',[3,1]))
-listPiecesWhite.append(Piece('R',[1,4]))
-listPiecesWhite.append(Piece('R',[1,1]))
 
-listPiecesBlack.append(Piece('q',[2,2]))
-listPiecesBlack.append(Piece('r',[2,1]))
-listPiecesBlack.append(Piece('r',[4,1]))
+w1=Piece('Q',[4,3])
+w2=Piece('R',[1,4])
+w3=Piece('R',[1,1])
+w4=Piece('N',[3,1])
+w5=Piece('N',[2,3])
+w6=Piece('R',[3,3])
+
+b1=Piece('q',[2,2])
+b2=Piece('r',[2,1])
+b3=Piece('r',[4,1])
+b4=Piece('n',[1,3])
+
+listPiecesWhite=[w1,w2,w3,w4,w5,w6]
+listPiecesBlack=[b1,b2,b3,b4]
 
 gameNode=GameNode(listPiecesWhite,listPiecesBlack,"white")
 
-#pairs=[['Q',[4,3]],['N',[3,1]],['R',[1,4]],['R',[1,1]],
-#       ['q',[2,2]],['r',[2,1]],['r',[4,1]]]
-
-#gameNode=GameNode.buildWithPairs(pairs,"white")
-#gameNode.draw()
-
 print "Check Rook Targets"
 print "Targets Black Rook (r) at [2,1] -> "
-for p in listTargetsRook(gameNode,Piece('r',[2,1])):    
+for p in listTargetsRook(gameNode,b2):    
     print p.type," ",p.coordenates
 
 print "Targets White Rook (R) at [1,1] -> "
-for p in listTargetsRook(gameNode,Piece('R',[1,1])):
+for p in listTargetsRook(gameNode,w3):
     print p.type," ",p.coordenates
 
-print "Targets White Rook (R) at [4,1] -> "
-print listTargetsRook(gameNode,Piece('r',[4,1]))
-for p in listTargetsRook(gameNode,Piece('r',[4,1])):
+print "Targets White Rook (r) at [4,1] -> "
+for p in listTargetsRook(gameNode,b3):
+    print p.type," ",p.coordenates
+
+
+print "Targets White Rook (R) at [3,3] -> "
+for p in listTargetsRook(gameNode,w6):
     print p.type," ",p.coordenates
