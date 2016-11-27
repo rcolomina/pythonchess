71967c776217b56328282510d85c032da10719cd
rcolomina <rcolomina@gmail.com>
2016-11-27 16:29:53 +0000

    Moved knight treatements to other file

--- functions.py
+++ functions.py
@@ -1,23 +1,7 @@
 from rookmovs import *
 from bishopmovs import *
 from queenmovs import *
-    
-# List of Knight movements 
-def listMovKnight(coord,ocupyCells):
-
-    maxsize=5 # max size gameboard
-    
-    l=[]
-    x=coord[0]
-    y=coord[1]
-    l=[[x+1,y+2],[x+2,y+1],[x+1,y-2],[x+2,y-1],[x-1,y-2],[x-2,y-1],[x-1,y+2],[x-2,y+1]]
-    
-    lfinal=[]
-    for p in l:
-        if p[0]>0 and p[0]<maxsize and p[1]>0 and p[1]<maxsize:
-            lfinal.append(p)
-
-    return [p for p in lfinal if p not in ocupyCells]
+from knightmovs import *
 
 # Accept a gamenode and a piece returning its list of movements
 def genListMovsPiece(gameNode,piece):
@@ -41,31 +25,30 @@ def genListMovsPiece(gameNode,piece):
         listMovsPiece=listMovKnight(coord,occupy)
 
     # Getting pieces for capturing movements
-    listTargetsPieces=[]
+    listTargetsPiece=[]
     if piece.type=="r" or piece.type=="R":
-        listTargetsPieces=listTargetsRook(gameNode,piece)
+        listTargetsPiece=listTargetsRook(gameNode,piece)
     if piece.type=="b" or piece.type=="B":
-        listTargetsPieces=listTargetsBishop(gameNode,piece)
+        listTargetsPiece=listTargetsBishop(gameNode,piece)
     if piece.type=="q" or piece.type=="Q":
-        listTargetsPieces=listTargetsQueen(gameNode,piece)
-#    if piece.type=="n" or piece.type=="N":
-#        listTargetsPieces=listTargetsKnight(gameNode,piece)
-                
+        listTargetsPiece=listTargetsQueen(gameNode,piece)
+    if piece.type=="n" or piece.type=="N":
+        listTargetsPiece=listTargetsKnight(gameNode,piece)
+
+    #print listTargetsPiece
+        
     # Adding posible capturing movements
-    for p in listTargetsPieces:
+    for p in listTargetsPiece:
         listMovsPiece.append(p.coordenates)
                     
     return listMovsPiece
 
 def checkPieceMovValid(gameNode,piece,mov):
     listMovsPiece=genListMovsPiece(gameNode,piece)
-    #print listMovsPiece
-    #print mov
     for m in listMovsPiece:
         if m==mov:
             return True
     return False
-
 
 
 def genListNextGameNodesForcingColor(gameNode,color):
