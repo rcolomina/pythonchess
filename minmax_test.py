6479821bcf53d731dd61051a9ee266f5940b4ae5
rcolomina <rcolomina@gmail.com>
2016-11-27 16:31:02 +0000

    Added test section to check out game succesors evolution

--- minmax_test.py
+++ minmax_test.py
@@ -3,6 +3,8 @@
 from piece import Piece
 from gameNode import GameNode
 from minmax import MinMax
+from functions import *
+
 
 listPiecesWhite=[]
 listPiecesBlack=[]
@@ -19,8 +21,41 @@ b1=Piece('q',[1,2])
 listPiecesBlack.append(b1)
 
 gameNode=GameNode(listPiecesWhite,listPiecesBlack,"white")
+succesors=genListNextGameNodes(gameNode)
 
-depth=2
-#print "Calculating MinMax: Sample 1"
-#print "Results minimax (0 draw / 1 white win / -1 blach win : ",MinMax(gameNode,depth,True)
+
+#n1=gameNode.succesGameActionMove(w1,[4,1])
+#n2=n1.succesGameActionMove(b1,[3,4])
+#n2.draw()
+#print "Knight movements:",genListMovsPiece(n2,Piece('N',[4,2]))
+
+#for s in succesors:
+#    print s.lastPieceMoved.type," ",s.lastPieceMoved.coordenates," lastMov:",s.lastMov
+#    secondSuccesors=genListNextGameNodes(s)
+#    for s2 in secondSuccesors:
+#        print "     ",s2.lastPieceMoved.type," ",s2.lastPieceMoved.coordenates," lastMov:",s2.lastMov," utility:",s2.utility()
+#        thirdSuccesors=genListNextGameNodes(s2)
+#        if len(thirdSuccesors)>0:
+#            nodeWiner=max(thirdSuccesors,key=lambda p: p.utility())
+#            print "            ",nodeWiner.lastPieceMoved.type," ",nodeWiner.lastPieceMoved.coordenates," lastMov:",nodeWiner.lastMov," utility:",nodeWiner.utility()
+
+        #for s3 in thirdSuccesors:
+         #   print "            ",s3.lastPieceMoved.type," ",s3.lastPieceMoved.coordenates," lastMov:",s3.lastMov," utility:",s3.utility()
+
+        
+    
+depth=3
+print "Calculating MinMax: Sample 1"
+print "Results minimax (0 draw / 1 white win / -1 blach win : ",MinMax(gameNode,depth,True)
 print MinMax(gameNode,depth,depth)
+
+
+
+
+
+
+
+
+
+
+
