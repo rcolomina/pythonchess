b1f90d22b5cfab688b02e52f76adbeb82a7b3f44
rcolomina <rcolomina@gmail.com>
2016-11-27 16:28:31 +0000

    checking game node

--- bishopmovs_test.py
+++ bishopmovs_test.py
@@ -6,20 +6,21 @@ from bishopmovs import *
 
 print "Checking GameNode class constructur"
 
-listPiecesWhite=[]
-listPiecesBlack=[]
+
 w1=Piece('Q',[2,2])
 w2=Piece('N',[3,2])
 w3=Piece('N',[4,2])
 w4=Piece('B',[1,3])
 w5=Piece('B',[2,4])
-
+listPiecesWhite=[]
 listPiecesWhite=[w1,w2,w3,w4,w5]
+
 
 b1=Piece('q',[1,2])
 b2=Piece('n',[3,1])
 b3=Piece('r',[1,1])
 b4=Piece('b',[4,4])
+listPiecesBlack=[]
 listPiecesBlack=[b1,b2,b3,b4]
 
 gameNode=GameNode(listPiecesWhite,listPiecesBlack,"black")
