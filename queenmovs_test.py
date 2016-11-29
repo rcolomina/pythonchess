40c22eb106838d0678a842cd2bf5e6c450685680
rcolomina <rcolomina@gmail.com>
2016-11-29 00:13:59 +0000

    Added assertion for the queen's list of targets

--- queenmovs_test.py
+++ queenmovs_test.py
@@ -2,7 +2,6 @@
 
 from piece import Piece
 from gameNode import GameNode        
-#from functions import *
 from minmax import MinMax
 
 from queenmovs import *
@@ -28,13 +27,17 @@ listPiecesBlack=[b1,b2,b3,b4]
 
 gameNode=GameNode(listPiecesWhite,listPiecesBlack,"white")
 
+gameNode.draw()
+
 print "Check Queen Targets"
 print "Targets Black (q) at [2,2] -> "
 for p in listTargetsQueen(gameNode,b1):
     print p.type," ",p.coordenates
 
-print "Targets White (Q) at [4,3] -> "
+pieces=[]
 for p in listTargetsQueen(gameNode,w1):
     print p.type," ",p.coordenates
-
-
+    pieces.append([p.type,p.coordenates])
+    
+assert(pieces==[['n', [1, 3]], ['r', [4, 1]], ['r', [2, 1]]])
+print "Assertion over white queen list of targets: ",pieces
